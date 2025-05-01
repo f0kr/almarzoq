42e1988f3d549e7f5a1f6cf3420aadd1b2714c75
f0kr <fukarfukar1@gmail.com>
2025-05-01 15:43:16 +0300

    params are awaited in route

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -2,10 +2,15 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function PATCH(req:Request, {params}:{params:{courseId:string}}) {
+export async function PATCH(req:Request, {
+    params
+}: Readonly<{
+  params: Promise<{ courseId: string }>
+
+}>) {
     try{
       const {userId} = await auth()
-      const {courseId} = params
+      const {courseId} = await params
       const values = await req.json()
 
       if(!userId) return new NextResponse("Unauthorized", { status: 401 })
