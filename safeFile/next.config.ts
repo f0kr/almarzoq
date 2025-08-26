1318f11983ed59dd6206ea41afb1b3d6e95e4807
f0kr <fukarfukar1@gmail.com>
2025-08-26 11:43:29 +0300

    for now nothing works with the postcss module error

---
+++ safeFile/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "lf9h2aefut.ufs.sh",
+      },
+    ]
+  },
+};
+
+export default nextConfig;
