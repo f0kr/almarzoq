b125fd27841d0b2c8e2267b087dc96f75e606ea9
f0kr <fukarfukar1@gmail.com>
2025-09-05 17:34:21 +0300

    enroll edited

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,6 +8,10 @@ import { Preview } from "@/components/Preview";
 import { Separator } from "@/components/ui/separator";
 import { File } from "lucide-react";
 import { CourseProgressButton } from "./_components/CourseProgressButton";
+import { Button } from "@/components/ui/button";
+import { formatPrice } from "@/lib/format";
+import toast from "react-hot-toast";
+import { SignIn, SignInButton } from "@clerk/nextjs";
 
 export default async function ChapterId({
     params
@@ -77,10 +81,24 @@ export default async function ChapterId({
                         isCompleted={!!userProgress?.isCompleted}
                         />
                       ) : (
-                        <CourseEnrollButton
+                        !userId ? (
+                         <p className=  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive">
+                          Please Sign in to enroll for {formatPrice(course.price!)}
+                          </p>
+                        ) : (
+                        <a target="_blank" href="https://t.me/AlmrzoqAcademy">
+                        <Button
+                        size="sm"
+                        className="w-full md:w-auto"
+                         >
+                        Contact Us and Enroll for {formatPrice(course.price!)}
+                        </Button>
+                        </a>
+                        )
+/*                         <CourseEnrollButton
                         courseId={courseId}
                         price={course.price!}
-                        />
+                        /> */
                       )}
                     </div>
                     <Separator/>
