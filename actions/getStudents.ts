58a90e2a6b5056c51eb0b4aff7653613578dc65b
f0kr <fukarfukar1@gmail.com>
2025-09-19 12:35:28 +0300

    still trying

--- actions/getStudents.ts
+++ actions/getStudents.ts
@@ -3,29 +3,22 @@ import { auth, clerkClient } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
 export async function getStudents() {
-  const { userId } = await auth()
 
-  if (!isTeacher(userId)) return redirect("/")
+    const {userId} = await auth()
+    
+        
+     if(!isTeacher(userId)) return redirect("/")
 
-  let allUsers: any[] = []
-  let hasMore = true
-
-  while (hasMore) {
-    const res = (await clerkClient()).users.getUserList({
-      limit: 1000,
-    })
-
-    allUsers = [...allUsers, ...(await res).data]
-
-    hasMore = (await res).data.length > 0
-  }
-
-  const students = allUsers.map((user) => ({
-    id: user.id,
-    fullName: user.fullName,
-    email: user.emailAddresses[0]?.emailAddress ?? "",
-    createdAt: user.createdAt,
+        const users = (await clerkClient()).users.getUserList()
+    
+        const students = (await users).data.map((user) => ({
+           id: user.id,
+           fullName: user.fullName,
+           email: user.emailAddresses[0]?.emailAddress ?? "",
+           createdAt: user.createdAt,
   }))
 
-  return students
+
+        return students
+
 }
