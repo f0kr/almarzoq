da4232e7c6d2817215d524820a4beada54448e0d
f0kr <fukarfukar1@gmail.com>
2025-11-25 02:55:10 +0300

    solving userIds

--- actions/getGroups.ts
+++ actions/getGroups.ts
@@ -1,14 +1,6 @@
-
-
 import { db } from "@/lib/db"
-import { isTeacher } from "@/lib/teacher"
-import { auth} from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
 
 export async function getGroups() {
-  const { userId } = await auth()
-  if (!isTeacher(userId)) return redirect("/")
-
 
   const groups = await db.groupUrl.findMany()
 
